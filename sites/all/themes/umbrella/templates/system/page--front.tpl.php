209d23fdb010f749a3242a2598e3390c835424b7
ramon <ramonltorres@gmail.com>
2015-04-16 13:40:44 -0400

    Revert "added toolbar to mobile homepage"
    This reverts commit 6744264d5ae7cf93e1ea52be67904e05fd261d4a.

--- sites/all/themes/umbrella/templates/system/page--front.tpl.php
+++ sites/all/themes/umbrella/templates/system/page--front.tpl.php
@@ -72,18 +72,12 @@
  *
  * @ingroup themeable
  */
-
-$isMobile = true; //check_user_agent('mobile');
-$mobileClass = ($isMobile) ? 'mobile' : '';
-
 ?>
-<?php if (!empty($page['info_home'])) { ?>
-<?php if(drupal_is_front_page() && !$isMobile) {  ?>
+<?php if (!empty($page['info_home'])): ?>
   <div class="section-home" id="home-section">
     <?php print render($page['info_home']); ?>
   </div>
-<?php } ?>
-<?php } ?>
+<?php endif; ?>
 <header class="clearfix" id="clsheader">
   <!-- Static navbar -->
   <div class="navbar navbar-default <?php print get_class_menu(); ?>">
@@ -127,13 +121,7 @@ $mobileClass = ($isMobile) ? 'mobile' : '';
 </header>
 
 <div class="main-container">
-  <?php 
-    if(drupal_is_front_page() && $isMobile) {
-        $html = views_embed_view('view_portfolio', 'block_1');
-        print $html;
-    } else {
 
-    ?>
   <header role="banner" id="page-header">
     <?php if (!empty($site_slogan)): ?>
       <p class="lead"><?php print $site_slogan; ?></p>
@@ -143,6 +131,7 @@ $mobileClass = ($isMobile) ? 'mobile' : '';
   </header> <!-- /#page-header -->
 
   <div class="main-content">
+
     <?php if (!empty($page['sidebar_first'])): ?>
       <aside class="col-sm-3" role="complementary">
         <?php print render($page['sidebar_first']); ?>
@@ -171,7 +160,6 @@ $mobileClass = ($isMobile) ? 'mobile' : '';
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
       <?php print render($page['content']); ?>
-
     </section>
 
     <?php if (!empty($page['sidebar_second'])): ?>
@@ -179,7 +167,7 @@ $mobileClass = ($isMobile) ? 'mobile' : '';
         <?php print render($page['sidebar_second']); ?>
       </aside>  <!-- /#sidebar-second -->
     <?php endif; ?>
-<?php } ?>
+
   </div>
 </div>
 <!-- footer 
--- sites/all/themes/umbrella/templates/system/page.tpl.php
+++ sites/all/themes/umbrella/templates/system/page.tpl.php
@@ -152,12 +152,7 @@
       <?php if (!empty($action_links)): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
-      <?php 
-
-print render($page['content']); 
-           
-    ?>
-
+      <?php print render($page['content']); ?>
     </section>
 
     <?php if (!empty($page['sidebar_second'])): ?>
